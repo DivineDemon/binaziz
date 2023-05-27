51716d8a68986e8a6c5816461ca41e00d62a744c
DivineDemon <supame123@gmail.com>
2023-05-27 18:26:23 +0500

    NavBar Done! 💄🚀✨

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav
+    class="w-screen fixed top-0 p-5 flex flex-row items-center justify-between bg-[#877f62]"
+  >
+    <!-- Logo -->
+    <div class="flex flex-row items-center justify-center space-x-5">
+      <font-awesome-icon icon="fa-solid fa-city" class="w-8 h-8" />
+      <span class="font-bold text-lg leading-5 cursor-pointer">
+        Bin Aziz Builtz
+      </span>
+    </div>
+    <!-- Items -->
+    <ul class="flex flex-row items-center justify-center space-x-10">
+      <li class="hover:border-b-2 hover:border-black cursor-pointer">Home</li>
+      <li class="hover:border-b-2 hover:border-black cursor-pointer">About</li>
+      <li class="hover:border-b-2 hover:border-black cursor-pointer">
+        Services
+      </li>
+      <li class="hover:border-b-2 hover:border-black cursor-pointer">
+        Contact
+      </li>
+    </ul>
+  </nav>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from "vue";
+import { MotionPlugin } from "@vueuse/motion";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faCity } from "@fortawesome/free-solid-svg-icons";
+
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
+import "./assets/css/tailwind.css";
 
-createApp(App).use(store).use(router).mount("#app");
+library.add(faCity);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(MotionPlugin)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
